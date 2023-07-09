1bd09cc2b8b6474b9aca0d7151faf060d8d82267
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-07-09 02:50:51 -0400

    JS-clock

--- src/clock.js
+++ src/clock.js
@@ -1,5 +1,17 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const min = String(date.getMinutes()).padStart(2, "0");
+  const sec = String(date.getSeconds()).padStart(2, "0");
+  const dayOrNight = date.getHours() > 11 ? "PM" : "AM";
+  clock.innerHTML = `${hours}:${min}:${sec} ${dayOrNight}`;
 }
+
+// function getClock() {
+//   clock.innerText = new Date().toLocaleTimeString();
+// }
+
+getClock();
+setInterval(getClock, 1000);
