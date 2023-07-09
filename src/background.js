888db04f9aade135eb7d5a746cabc90e54490ecc
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-07-09 15:35:01 -0400

    JS-quote-bg

---
+++ src/background.js
@@ -0,0 +1,19 @@
+const background = [
+  "0.jpeg",
+  "1.jpeg",
+  "2.jpeg",
+  "3.jpeg",
+  "4.jpeg",
+  "5.jpeg",
+  "6.jpeg",
+  "7.jpeg",
+  "8.jpeg",
+  "9.jpeg",
+];
+
+const image = background[Math.floor(Math.random() * background.length)];
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${image}`;
+
+document.body.appendChild(bgImage);
---
+++ src/quotes.js
@@ -0,0 +1,73 @@
+const quotes = [
+  {
+    quote:
+      "You will face many defeats in life, but never let yourself be defeated.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote:
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+    author: "Abraham Lincoln",
+  },
+  {
+    quote:
+      "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+    author: "Thomas Edison",
+  },
+  {
+    quote:
+      "If you spend too much time thinking about a thing, you'll never get it done.",
+    author: "Bruce Lee",
+  },
+  {
+    quote: "If you would be loved, love and be lovable.",
+    author: "Benjamin Franklin",
+  },
+  {
+    quote:
+      "Success is going from failure to failure without a loss of enthusiasm.",
+    author: "Winston Churchill",
+  },
+  {
+    quote:
+      "Always bear in mind that your own resolution to succeed is more important than any other.",
+    author: "Abraham Lincoln",
+  },
+  {
+    quote:
+      "Try not to become a man of success but rather try to become a man of value.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "I never dreamed about success, I worked for it.",
+    author: "Estee Lauder",
+  },
+  {
+    quote:
+      "If you cannot fly then run. If you cannot run, then walk. And, if you cannot walk, then crawl, but whatever you do, you have to keep moving forward.",
+    author: "Martin Luther King Jr",
+  },
+  {
+    quote:
+      "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    author: "Thomas Edison",
+  },
+  {
+    quote:
+      "The only thing worse than starting something and failing … is not starting something.",
+    author: "Seth Godin",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerHTML = todaysQuote.quote;
+author.innerHTML = todaysQuote.author;
